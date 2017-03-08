7fa5dc354127468ca3834f81eccccde2bb76eba7
Geoff Ereth <geoffereth@gmail.com>
2017-03-08 14:44:42 -0800

    wip

--- lib/right_signature/client/sending_requests.rb
+++ lib/right_signature/client/sending_requests.rb
@@ -5,17 +5,17 @@ module RightSignature
         get "sending_requests/#{sending_request_id}"
       end
 
-      def start_sending_request(query = {})
+      def sending_request_start(query = {})
         post "sending_requests", query
       end
 
-      def upload_sending_request_file(path, file_path, content_type = "application/pdf")
+      def sending_request_upload_file(path, file_path, content_type = "application/pdf")
         file = Faraday::UploadIO.new(file_path, content_type)
         conn = Faraday.new
         conn.put path, {file: file}
       end
 
-      def finish_sending_request(sending_request_id)
+      def sending_request_finish(sending_request_id)
         post "sending_requests/#{sending_request_id}/uploaded", {}
       end
     end
